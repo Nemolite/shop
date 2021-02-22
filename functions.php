6e6ab23c792f422b8f48a136ec7e69b51cdb233d
Nemolite <g25092011@mail.ru>
2021-02-22 19:32:37 +0300

    Тестовый вывод строки

--- functions.php
+++ functions.php
@@ -4,3 +4,15 @@
  * 
  */
 ?>
+<?php
+
+if ( ! function_exists( 'storefront_is_woocommerce_activated' ) ) {
+	/**
+	 * Тестовый вывод данных
+	 */
+	function test_function_show() {
+		echo "test";
+	}
+}
+add_action( 'woocommerce_before_main_content','test_function_show',30 );
+?>
