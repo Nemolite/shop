aa960e4136497745f597a471d9508f4464ab208c
Nemolite <g25092011@mail.ru>
2021-02-22 20:38:07 +0300

    Тестовый вывод стилей с помощью хуков

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -29,7 +29,6 @@ get_header( 'shop' );
 do_action( 'woocommerce_before_main_content' );
 
 ?>
-test
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
