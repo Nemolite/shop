3f7a894528ea96ddfb6f67d0538c20f0e2b18585
Nemolite <g25092011@mail.ru>
2021-02-25 09:17:10 +0300

    Подключение своих файлов стиля и скриптов

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ function myshop_register_styles() {
 	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/assets/css/font-awesome.min.css', array(), $theme_version );
 	wp_enqueue_style( 'owl-carousel', get_stylesheet_directory_uri() . '/assets/css/owl.carousel.css', array(), $theme_version );
 	wp_enqueue_style( 'responsive', get_stylesheet_directory_uri() . '/assets/css/responsive.css', array(), $theme_version );
+	wp_enqueue_style( 'mystyle', get_stylesheet_directory_uri() . '/assets/css/mystyle.css', array(), $theme_version );
 
 }
 
@@ -37,6 +38,7 @@ function myshop_register_scripts() {
 	wp_enqueue_script( 'sticky-js', get_stylesheet_directory_uri() . '/assets/js/jquery.sticky.js', array(), $theme_version, true );
 	wp_enqueue_script( 'easing-js', get_stylesheet_directory_uri() . '/assets/js/jquery.easing.1.3.min.js', array(), $theme_version, true );
 	wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array(), $theme_version, true );
+	wp_enqueue_script( 'myscript-js', get_stylesheet_directory_uri() . '/assets/js/myscript.js', array(), $theme_version, true );
 
 	
 
