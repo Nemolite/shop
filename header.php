dac412c129b62c44dc9af02562ce74fb92338ba9
Nemolite <g25092011@mail.ru>
2021-04-13 15:38:27 +0300

    Добавлен слайдер на главной странице

--- header.php
+++ header.php
@@ -146,7 +146,5 @@
 	do_action( 'storefront_before_content' );
 	?>
 
-	<div id="content" class="site-content" tabindex="-1">
-		<div class="col-full">
-		<?php
+	<?php
 		do_action( 'storefront_content_top' );
