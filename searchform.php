3f516d609b2afcfc6413baccaa0b57eeced9ac5f
Nemolite <g25092011@mail.ru>
2022-08-16 11:15:51 +0300

    search

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
+	<label class="screen-reader-text" for="s">Поиск: </label>
+	<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search products..." />
+    
+	<input type="submit" id="searchsubmit" value="Search" />
+</form>
+
--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -26,10 +26,7 @@ global $product;
                 <div class="col-md-4">
                     <div class="single-sidebar">
                         <h2 class="sidebar-title">Search Products</h2>
-                        <form action="">
-                            <input type="text" placeholder="Search products...">
-                            <input type="submit" value="Search">
-                        </form>
+                        <?php get_search_form(); ?>
                     </div>
                     
                     <div class="single-sidebar">
